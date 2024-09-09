515e6e8ce39f4258a572ec554df3e10065bcf66f
Katia Bernardes <katia.bernardes@codeminer42.com>
2024-09-09 08:50:43 -0300

    feat: add shoppingList initial components

--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -1,5 +1,31 @@
+import './ShoppingList.css';
+
 const ShoppingList = () => {
-  return <p>Test</p>;
+  return (
+    <>
+      <h2 className="list__title">Shopping list</h2>
+      <div className="list__container">
+        <div>
+          <div className="list__container__input">
+            <input />
+            <button>+</button>
+          </div>
+
+          {/* <div className="list__container__item">
+            <p>Item</p>
+            <button>+3</button>
+            <button>+</button>
+            <button>-</button>
+            <button>-3</button>
+          </div> */}
+        </div>
+        <div className="list__container__list">
+          <h3>List</h3>
+          <button>Add random list</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShoppingList;
